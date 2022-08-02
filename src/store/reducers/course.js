0ef61f7c957482d7b372f06e3898643d9532d4f3
mateus.oliveira <mateus.oliveira@dtidigital.com.br>
2022-08-02 15:28:01 -0300

    first commit

---
+++ src/store/reducers/course.js
@@ -0,0 +1,30 @@
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com React",
+      lessons: [
+        { id: 1, title: "Primeira aula" },
+        { id: 2, title: "Segunda aula" }
+      ]
+    },
+    {
+      id: 2,
+      title: "Aprendendo React",
+      lessons: [
+        { id: 3, title: "Terceira aulba" },
+        { id: 4, title: "Quarta aula" }
+      ]
+    }
+  ]
+};
+
+export default function course(state = INITIAL_STATE, action) {
+  if (action.type === "TOGGLE_LESSON") {
+    return { ...state, activeLesson: action.lesson, activeModule: action.module };
+  }
+
+  return state;
+}
